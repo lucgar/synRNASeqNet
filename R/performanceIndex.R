7e1e6085dd805506a4a4a312ff507dd52ff4b936
lucgar <lucianogarofano88@gmail.com>
2015-04-09 15:02:14 +0200

    Remove hidden functions problem

--- R/performanceIndex.R
+++ R/performanceIndex.R
@@ -1,20 +1,20 @@
 performanceIndex <-
 function(testNet, gsNet){
-    ans <- validate(testNet, gsNet)
-    
-    nEdges <- ans$tp + ans$fp
-    recall <- ans$tp/(ans$tp + ans$fn)
-    fpr <- ans$fp/(ans$fp + ans$tn)
-    precision <- ans$tp/(ans$tp + ans$fp)
-    precision[is.na(precision)] <- 0
-    accuracy <- (ans$tp + ans$tn)/(ans$tp + ans$fn + ans$fp + ans$tn)
-    Fscore <- 2*(precision*recall)/(precision + recall)
-    Fscore[is.na(Fscore)] <- 0
-    
-    ans <- cbind(ans[, 1], nEdges, ans[, -1], recall, fpr, precision,
-                 accuracy, Fscore)
-    colnames(ans) <- c("Thresh", "nEdges", "TP", "FP", "FN", "TN",
-                       "Recall", "FPR", "Precision", "Accuracy", "Fscore")
-    
-    return(ans)
-  }
+  ans <- validate(testNet, gsNet)
+  
+  nEdges <- ans$tp + ans$fp
+  recall <- ans$tp/(ans$tp + ans$fn)
+  fpr <- ans$fp/(ans$fp + ans$tn)
+  precision <- ans$tp/(ans$tp + ans$fp)
+  precision[is.na(precision)] <- 0
+  accuracy <- (ans$tp + ans$tn)/(ans$tp + ans$fn + ans$fp + ans$tn)
+  Fscore <- 2*(precision*recall)/(precision + recall)
+  Fscore[is.na(Fscore)] <- 0
+  
+  ans <- cbind(ans[, 1], nEdges, ans[, -1], recall, fpr, precision,
+               accuracy, Fscore)
+  colnames(ans) <- c("Thresh", "nEdges", "TP", "FP", "FN", "TN",
+                     "Recall", "FPR", "Precision", "Accuracy", "Fscore")
+  
+  return(ans)
+}
--- R/plotROC.R
+++ R/plotROC.R
@@ -1,7 +1,7 @@
 plotROC <-
 function(piNet, ...){
-    plot(piNet[, "FPR"], piNet[, "Recall"], type = "l",
-         xlab = "FP rate", ylab = "TP rate", main = "ROC Curve",
-         xlim = 0:1, ylim = 0:1, ...)
-    lines(0:1, 0:1, col = "black")
-  }
+  plot(piNet[, "FPR"], piNet[, "Recall"], type = "l",
+       xlab = "FP rate", ylab = "TP rate", main = "ROC Curve",
+       xlim = 0:1, ylim = 0:1, ...)
+  lines(0:1, 0:1, col = "black")
+}
